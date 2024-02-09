033ad60d45ba640d86363562b96b37ac0f56a013
matteomiceli <omatteomiceli@gmail.com>
2024-02-08 23:56:46 -0800

    chore: style nav bar

--- frontend/src/components/Navbar.svelte
+++ frontend/src/components/Navbar.svelte
@@ -1,5 +1,8 @@
 <script lang="ts">
   import { nav, type Nav } from "../stores/nav";
+  import Anvil from "./icons/Anvil.svelte";
+  import Book from "./icons/Book.svelte";
+  import Runik from "./icons/Runik.svelte";
 
   const navigate = (location: Nav) => {
     nav.set(location);
@@ -7,6 +10,39 @@
 </script>
 
 <nav>
-  <button on:click={() => navigate("lib")}>Lib</button>
-  <button on:click={() => navigate("gen")}>Gen</button>
+  <button on:click={() => navigate("lib")}><Runik size="24px" /></button>
+  <button
+    class={`${$nav === "lib" && "active"}`}
+    on:click={() => navigate("lib")}
+    ><Book
+      size="24px"
+      color={`${$nav === "lib" ? "black" : "#5D5D5D"}`}
+    /></button
+  >
+  <button
+    class={`${$nav === "gen" && "active"}`}
+    on:click={() => navigate("gen")}
+    ><Anvil
+      size="32px"
+      color={`${$nav === "gen" ? "black" : "#5D5D5D"}`}
+    /></button
+  >
 </nav>
+
+<style>
+  nav {
+    height: 100%;
+    display: flex;
+    justify-content: start;
+    align-items: center;
+  }
+  button {
+    width: 72px;
+    height: 100%;
+    background-color: transparent;
+    border: none;
+  }
+  .active {
+    background-color: white;
+  }
+</style>
