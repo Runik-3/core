787df9d08742915fd5246790eb06662e0dc76a35
matteomiceli <omatteomiceli@gmail.com>
2024-02-11 22:38:52 -0800

    chore: cleanup devices panel

--- frontend/src/components/DevicePanel.svelte
+++ frontend/src/components/DevicePanel.svelte
@@ -1,5 +1,7 @@
 <script lang="ts">
   import { SelectDevice } from "../../wailsjs/go/main/App";
+  import Button from "./Button.svelte";
+  import Device from "./icons/Device.svelte";
   let devicePath = "";
 
   const openDir = async () => {
@@ -8,10 +10,31 @@
 </script>
 
 <div>
-  <p>
-    {devicePath
-      ? `Device Selected: ${devicePath}`
-      : "Please choose your e-reader device."}
-  </p>
-  <button on:click={openDir}>Select Reader Device</button> <br />
+  <h3>Devices</h3>
+  <div class="content">
+    <Device size="128px" color="#5D5D5D" />
+    <p>
+      {devicePath
+        ? `Device Selected: ${devicePath}`
+        : "Please choose your e-reader device."}
+    </p>
+    <Button onClick={openDir} small>Select Reader Device</Button>
+  </div>
 </div>
+
+<style>
+  div {
+    padding: 24px;
+  }
+  .content {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+  h3 {
+    margin-bottom: 24px;
+  }
+  p {
+    margin: 24px 0;
+  }
+</style>
