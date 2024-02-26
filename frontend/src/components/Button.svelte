137c4b6f1ee0ecacf64f0456b90b7ac78de511c2
matteomiceli <omatteomiceli@gmail.com>
2024-02-25 18:08:58 -0800

    feat: button variants

--- frontend/src/components/Button.svelte
+++ frontend/src/components/Button.svelte
@@ -1,12 +1,15 @@
 <script lang="ts">
+  type ButtonType = "primary" | "secondary" | "error"
+
   export let onClick: (...args: any[]) => void;
+  export let type: ButtonType = "primary"
   export let disabled = false;
   export let small = false;
   export let maxWidth = false;
 </script>
 
 <button
-  class={`${small && "small"} ${maxWidth && "max-width"}`}
+  class={`${small && "small"} ${maxWidth && "max-width"} ${type}`}
   on:click={onClick}
   {disabled}
 >
@@ -16,14 +19,25 @@
 <style>
   button {
     background-color: black;
+    box-sizing: border-box;
     color: white;
-    border: none;
+    border: 2px black solid;
     border-radius: 8px;
     padding: 0 32px;
     max-width: 256px;
     height: 48px;
     font-size: 1rem;
     cursor: pointer;
+  }
+  .secondary {
+    background-color: transparent; 
+    border: 2px black solid;
+    color: black;
+  }
+  .error {
+    background-color: #c76767; 
+    border: 2px #c76767 solid;
+    color: white;
   }
   .small {
     padding: 0 24px;
