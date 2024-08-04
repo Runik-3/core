cb3b2196e7f617439f94c6ec720d9c5e118baccc
matteomiceli <omatteomiceli@gmail.com>
2024-08-04 01:01:16 -0700

    fix: release script writing to file

--- release.sh
+++ release.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+GREEN=$'\e[0;32m'
+RESET=$'\e[0m'
+
 if [[ -z $1 ]];
   then
   echo "please provide a release version"
@@ -7,8 +10,12 @@ fi
 
 if [[ $(git describe --tags) != $1 ]];
 then 
-  $1 > version
+  echo $1 > version
   git add version
   git commit -m "build: $1"
   git tag $1
+
+  echo "$GREEN
+$1 created. To finalize release, push the tag with$RESET
+  git push tag origin [tagname]"
 fi
