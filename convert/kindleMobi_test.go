75acd38e67abb4d14f68e0dc48e7d7ae30133d03
matteomiceli <omatteomiceli@gmail.com>
2026-02-16 20:43:17 -0800

    test: kindle epub conversion
    tests the epub shape required for kindlegen ingestion

---
+++ convert/kindleMobi_test.go
@@ -0,0 +1,48 @@
+package convert
+
+import (
+	"bytes"
+	"fmt"
+	"os"
+	"path/filepath"
+	"testing"
+)
+
+// At the risk of testing an external lib, this tests the intermediary ebub
+// shape the dictionary gets converted to before being processes by kindlegen.
+func TestConvertKindleMobi(t *testing.T) {
+	tempDir, err := os.MkdirTemp("", "runik_temp_test_dir")
+	if err != nil {
+		t.Fatalf("Failed to create temp testing dir\n")
+	}
+
+	_, err = writeEpub(testDict, tempDir)
+	if err != nil {
+		t.Fatalf("Failed to convert dict\n")
+	}
+	convertedDictDir := filepath.Join(tempDir, "temp", "epub")
+	fmt.Println("dict dir", convertedDictDir)
+
+	convertedWords := readFileData(filepath.Join(convertedDictDir, "OEBPS", "html", "words.xhtml"))
+	expectedWords := getFixtureData(filepath.Join("kindleEpub", "words.xhtml"))
+	if !bytes.Equal(convertedWords, expectedWords) {
+		t.Fatalf(
+			"Kindle epub: converted words.xhtml does not match expected\n\nconverted: \n%b\n\nexpected: \n%b",
+			convertedWords,
+			expectedWords,
+		)
+	}
+
+	convertedOpf := readFileData(filepath.Join(convertedDictDir, "OEBPS", "content.opf"))
+	expectedOpf := getFixtureData(filepath.Join("kindleEpub", "content.opf"))
+	if !bytes.Equal(convertedOpf, expectedOpf) {
+		t.Fatalf(
+			"Kindle epub: converted opf file does not match expected\n\nconverted: \n%b\n\nexpected: \n%b",
+			convertedOpf,
+			expectedOpf,
+		)
+	}
+
+	// Cleanup
+	os.RemoveAll(tempDir)
+}
